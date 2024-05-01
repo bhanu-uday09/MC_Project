0ed00e1e2553cf8ee3cfeced30123dc85b1124c9
Bhanu Uday <140607545+bhanu-uday09@users.noreply.github.com>
2024-05-01 19:58:58 +0530

    Linked the Backend to fetch News

--- app/src/main/java/com/msr_mc24/mc_newsapp/NewsDisplay.kt
+++ app/src/main/java/com/msr_mc24/mc_newsapp/NewsDisplay.kt
@@ -19,20 +19,32 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -88,14 +100,61 @@ fun NewsScreen(apiInterface: ApiInterface, category: String) {
     }
 
     Surface {
-        if (loading) {
-            // Show loading indicator
-            Text(text = "Loading...")
-        } else {
-            // Show news articles
-            LazyColumn {
-                items(newsList) { newsArticle ->
-                    NewsArticleCard(newsArticle)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .background(color = Color(0xFFB9A6E4), shape = RoundedCornerShape(6.dp))
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                        .wrapContentWidth(align = Alignment.CenterHorizontally)
+                ) {
+                    val imageResource = when (category) {
+                        "business" -> R.drawable.business
+                        "technology" -> R.drawable.tech
+                        "entertainment" -> R.drawable.entertainment
+                        "sports" -> R.drawable.sport
+                        "health" -> R.drawable.health
+                        "science" -> R.drawable.science
+                        // Add more cases for each category
+                        else -> R.drawable.headlines // Provide a default image if category doesn't match any known category
+                    }
+                    Image(
+                        painter = painterResource(id = imageResource),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(60.dp)
+                            .padding(5.dp)
+                    )
+                    Text(
+                        text = category.uppercase(),
+                        fontSize = 28.sp,
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        fontFamily = FontFamily.Monospace,
+                        modifier = Modifier
+                            .padding(4.dp, 12.dp, 23.dp, 10.dp)
+                    )
+                }
+            }
+
+            // Display news articles
+            if (loading) {
+                // Show loading indicator
+                CircularProgressIndicator()
+            } else {
+                // Show news articles
+                LazyColumn {
+                    items(newsList) { newsArticle ->
+                        NewsArticleCard(newsArticle, category)
+                    }
                 }
             }
         }
@@ -103,7 +162,7 @@ fun NewsScreen(apiInterface: ApiInterface, category: String) {
 }
 
 @Composable
-fun NewsArticleCard(newsArticle: NewsArticle) {
+fun NewsArticleCard(newsArticle: NewsArticle, category: String) {
     val context = LocalContext.current
     val intent = Intent(context, Description::class.java).apply {
         putExtra("title", newsArticle.title)
@@ -116,20 +175,27 @@ fun NewsArticleCard(newsArticle: NewsArticle) {
                     .padding(8.dp)
                     .clickable(onClick = {
                         context.startActivity(intent)
-                    }),
-//                elevation = 8.dp
+                    })
             ) {
                 Column(
                     modifier = Modifier.padding(16.dp)
                 ) {
-                    Text(text = newsArticle.title ?: "No Title", fontWeight = FontWeight.Bold, style = MaterialTheme.typography.headlineMedium, fontSize = 27.sp)
+                    Text(
+                        text = newsArticle.title ?: "No Title",
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.headlineMedium,
+                        fontSize = 27.sp
+                    )
                     Spacer(modifier = Modifier.height(8.dp))
-                    Text(text = "${newsArticle.author ?: "Unknown"}", style = MaterialTheme.typography.bodySmall)
-                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "${newsArticle.author ?: "Unknown"}",
+                        style = MaterialTheme.typography.bodySmall
+                    )
+
 //                    Text(text = newsArticle.description ?: "No Description", style = MaterialTheme.typography.bodyMedium)
                 }
             }
+            Divider(modifier = Modifier.height(1.dp))
         }
-
     }
 }
